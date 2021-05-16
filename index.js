e4918204c76048f87a0d99fb0ea16342368299a9
Halima AlRajaibi <61138040+HalimaHamood@users.noreply.github.com>
2021-05-16 15:47:22 +0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,30 @@
+
+const menuBtn = document.querySelector('.menu')
+      panel = document.querySelector('.filter')
+      panelStyles = window.getComputedStyle(panel)
+    //   https://developer.mozilla.org/en-US/docs/Web/API/Window/getComputedStyle
+
+      hearts = document.querySelectorAll('.far')
+            
+menuBtn.addEventListener('click', () => {
+    
+    if (panelStyles.getPropertyValue('clip-path') === 'circle(0% at 100% 0%)') {
+        panel.style.clipPath = 'circle(100% at 50% 50%)'
+    }
+    
+    else {
+        panel.style.clipPath = 'circle(0% at 100% 0%)'
+    }
+})
+
+document.addEventListener('mouseup', (event) => {
+    if (panelStyles.getPropertyValue('clip-path') === 'circle(100% at 50% 50%)') {
+        if(!panel.contains(event.target))
+            panel.style.clipPath = 'circle(0% at 100% 0%'
+    }
+})
+
+function changeHeart(x) {
+    x.classList.toggle("fas")
+    x.classList.toggle("far")
+}
